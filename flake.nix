326b704b8b74692c6f1ed514299648e05a3e0549
Aly Raffauf <aly@raffauflabs.com>
2024-07-05 22:42:32 -0400

    convert to nix flake

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "Aly's NixOS flake.";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+  };
+
+  outputs = inputs @ {self, ...}: {
+    formatter = inputs.nixpkgs.lib.genAttrs [
+      "aarch64-darwin"
+      "aarch64-linux"
+      "x86_64-darwin"
+      "x86_64-linux"
+    ] (system: inputs.nixpkgs.legacyPackages.${system}.alejandra);
+
+    packages =
+      inputs.nixpkgs.lib.genAttrs [
+        "aarch64-linux"
+        "x86_64-linux"
+      ] (system: {
+        default = inputs.nixpkgs.legacyPackages."${system}".stdenv.mkDerivation {
+          name = "wallpapers";
+          version = "0.1.0";
+          src = ./.;
+          buildInputs = [];
+          buildPhase = "";
+          installPhase = ''
+            mkdir -p $out/share/backgrounds
+            cp -r *.jpg $out/share/backgrounds
+            cp -r *.png $out/share/backgrounds
+          '';
+        };
+      });
+  };
+}
